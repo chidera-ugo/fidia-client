7c14df6f4484e99eb97df517f5b1e3526362b488
chidera-ugo <chideraugoeresimadu@gmail.com>
2022-02-14 05:34:22 +0100

    oscillator

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -20,7 +20,7 @@ export const prefillDateRange = (
     if (resultMap[presentDate.toISOString()]) {
       summary.push(resultMap[presentDate.toISOString()])
     } else {
-      summary.push({ date: presentDate.toISOString(), count: 52 })
+      summary.push({ date: presentDate.toISOString(), count: 36 })
     }
 
     presentDate.setDate(presentDate.getDate() - 1)
